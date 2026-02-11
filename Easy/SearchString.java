7c43f89b1edebb655eda03992e75d6b843a866d7
Warren <warrenandrade@live.com>
2026-02-10 20:30:46 -0500

    Leetcode question 35: Search insert position correct solution

--- Easy/SearchString.java
+++ Easy/SearchString.java
@@ -13,11 +13,8 @@ public class SearchString {
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == target){
                 return i;
-            }else if(nums[i] < target){
-                return i;
             }
         }
-
-        return 0;
+        return nums.length;
     }
 }
