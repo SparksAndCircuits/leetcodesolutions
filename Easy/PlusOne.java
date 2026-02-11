b668ca773430dae984bcbdc7dd0fd6d3b2046465
Warren <warrenandrade@live.com>
2026-02-10 20:59:54 -0500

    Leetcode question 66: Plus one

---
+++ Easy/PlusOne.java
@@ -0,0 +1,24 @@
+package Easy;
+
+/* Leetcode question 66: Plus one
+
+    You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+    Increment the large integer by one and return the resulting array of digits.
+
+*/
+
+public class PlusOne {
+    public int[] plusOne(int[] digits){
+        for(int i = 0; i < digits.length; i++){
+            if(i == digits.length){
+                if(digits[i] == 9){
+                    digits[i] = 0;
+                    digits[i - 1] += 1;
+                }
+            }
+        }
+
+        return digits;
+    }
+}
