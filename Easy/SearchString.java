f9a82950febb0b30fb9f4b3c5557bc490fd0710b
Warren <warrenandrade@live.com>
2026-02-10 20:30:14 -0500

    Leetcode question 35: Search insert position

---
+++ Easy/SearchString.java
@@ -0,0 +1,23 @@
+package Easy;
+
+/*  Leetcode question 35: Search insert position
+
+    Given a sorted array of distinct integers and a target value, return teh index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+*/
+
+public class SearchString {
+    public int searchString(int[] nums, int target){
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            }else if(nums[i] < target){
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
