38f9babd0aa12cb9a9dea040be31a441a0c1be30
Warren <warrenandrade@live.com>
2026-03-01 17:55:23 -0500

    Leetcode question 101: Symmetric Tree

--- Easy/SymmetricTree/IsSymmetric.java
+++ Easy/SymmetricTree/IsSymmetric.java
@@ -10,17 +10,18 @@ public class IsSymmetric {
     public TreeNode left;
 
     public boolean isSymmetric(TreeNode root){
-        if(root == null){
-            return false;
-        }
+        return dfs(root.left, root.right);
+    }
 
-        if(root.left == root.right){
+    private boolean dfs(TreeNode left, TreeNode right){
+        if(left == null && right == null){
             return true;
         }
 
-        isSymmetric(root.left);
-        isSymmetric(root.right);
-        return false;
-    }
-    
+        if(left == null || right == null){
+            return false;
+        }
+
+        return (left.val == right.val && dfs(left.left, right.right) && dfs(left.right, right.left));
+    }    
 }
